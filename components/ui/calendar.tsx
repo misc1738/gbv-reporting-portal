18b4c06b3fb3534fcd9ea6e968899a44a5fd7002
misc1738 <dkimorata@usiu.ac.ke>
2025-11-29 06:54:59 +0300

    feat: add react-day-picker stylesheet import

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import { DayPicker } from "react-day-picker"
+import "react-day-picker/style.css"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
